27a2395b0a55399113ccc5bfddf90e0da15d001a
Oliveira223 <162735266+Oliveira223@users.noreply.github.com>
2025-03-22 01:40:54 +0000

    nova página de informações

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ window.onload = function () {
 
 const icons = document.querySelectorAll('.icon');
 const overlay = document.querySelector('.overlay');
+const overlayLinks = document.querySelectorAll('.overlay a');
 const icon = document.querySelector('.icon.nav-icon-5');
+const navIcon = document.querySelector('.nav-icon-5');
 const navMenu = document.querySelector('.nav-menu');
 
 icons.forEach(icon => {
@@ -19,6 +21,15 @@ icons.forEach(icon => {
 
         // Alterna a classe 'open' no menu de navegação
         navMenu.classList.toggle('open');
+    });
+});
+
+
+// Adiciona um evento de clique em cada link
+overlayLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        overlay.classList.remove('open'); // Fecha o overlay
+        navIcon.classList.remove('open'); // Reseta o ícone do menu
     });
 });
 
